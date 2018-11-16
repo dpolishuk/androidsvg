7de012db0a4876e7908759c4410f1d9aae53e350
Dmitry Polishuk <deepol@yandex-team.ru>
2018-11-16 10:53:56 +0300

    added androidsvg

---
+++ src/androidTest/java/com/caverock/androidsvg/CanvasLegacyTest.java
@@ -0,0 +1,14 @@
+package com.caverock.androidsvg;
+
+import android.graphics.Canvas;
+import org.junit.Test;
+
+public class CanvasLegacyTest {
+
+    @Test
+    public void testSave() {
+        final Canvas canvas = new Canvas();
+        CanvasLegacy.save(canvas, CanvasLegacy.MATRIX_SAVE_FLAG);
+        canvas.restore();
+    }
+}
